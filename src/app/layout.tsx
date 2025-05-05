8f875f90a024322dc23e2b14c01294ecff5a7f08
Ahmed Elsakaan <ahmed@elsakaan.me>
2025-05-05 17:14:31 +0100

    remove geist font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,9 @@
 import './globals.css'
 
-import { Geist, Geist_Mono } from 'next/font/google'
-
 import type { Metadata } from 'next'
 import type { PropsWithChildren } from 'react'
 
 import { ThemeProvider } from 'next-themes'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -25,9 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <ThemeProvider attribute='class' disableTransitionOnChange>
           {children}
         </ThemeProvider>
